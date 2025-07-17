1fe00c6cd343835147470682351d660054f1e929
Isaac <isaacchu888@gmail.com>
2025-07-17 03:03:03 -0700

    legal move checking

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main() -> None:
     board = Board()
     board.pretty_print(coords=True)
     print("Potential moves for black:", board.potential_moves[0])
+    print("Legal moves for black:", board.legal_moves[0])
 
 if __name__ == "__main__":
     main()
--- src/board.py
+++ src/board.py
@@ -10,7 +10,33 @@ class Board:
         # Black's pieces are represented by self.state[0] to begin
         self.state: np.ndarray = Board.__convert_to_state(get_config("config.yaml").board.starting_position)
         # Stores the set of empty squares that neighbor an opponent's piece
-        self.potential_moves: Tuple[Set[Tuple[int, int]], Set[Tuple[int, int]]] = Board.__initialize_potential_moves(self.state)
+        self.potential_moves: Tuple[Set[Tuple[int, int]], Set[Tuple[int, int]]]
+        self.__initialize_potential_moves()
+        # Stores the set of legal moves for each player
+        self.legal_moves: Tuple[Set[Tuple[int, int]], Set[Tuple[int, int]]]
+        self.__initialize_legal_moves()
+
+    def __is_legal_move(self, move: Tuple[int, int]) -> bool:
+        # Assumes that the move is in bounds
+        # Check if the move is in the set of potential moves (also ensures that the square is empty)
+        if move not in self.potential_moves[0] and move not in self.potential_moves[1]:
+            return False
+        # Check if the move captures any opponent's pieces
+        for di, dj in Board.DIRECTIONS:
+            ni, nj = move[0] + di, move[1] + dj
+            found_opponent = False
+            while 0 <= ni < 8 and 0 <= nj < 8:
+                if self.state[1, ni, nj] > 0:
+                    found_opponent = True
+                    ni += di
+                    nj += dj
+                    continue
+                if self.state[0, ni, nj] > 0:
+                    if found_opponent:
+                        return True
+                    break
+                break
+        return False
 
     def pretty_print(self, coords: bool = False) -> None:
         board_repr: List[List[str]] = []
@@ -47,11 +73,10 @@ class Board:
         state: np.ndarray = np.array([black, white], dtype=np.float32)
         return state
     
-    @staticmethod
-    def __initialize_potential_moves(state: np.ndarray) -> Tuple[Set[Tuple[int, int]], Set[Tuple[int, int]]]:
+    def __initialize_potential_moves(self) -> None:
         black_potential: Set[Tuple[int, int]] = set()
         white_potential: Set[Tuple[int, int]] = set()
-        all_pieces: np.ndarray = state[0] + state[1]
+        all_pieces: np.ndarray = self.state[0] + self.state[1]
         for i in range(8):
             for j in range(8):
                 if all_pieces[i, j] != 0:
@@ -60,8 +85,19 @@ class Board:
                     ni, nj = i + di, j + dj
                     if ni < 0 or ni >= 8 or nj < 0 or nj >= 8:
                         continue
-                    if state[1, ni, nj] > 0:
+                    if self.state[1, ni, nj] > 0:
                         black_potential.add((i, j))
-                    if state[0, ni, nj] > 0:
+                    if self.state[0, ni, nj] > 0:
                         white_potential.add((i, j))
-        return black_potential, white_potential
+        self.potential_moves = (black_potential, white_potential)
+
+    def __initialize_legal_moves(self) -> None:
+        black_legal: Set[Tuple[int, int]] = set()
+        white_legal: Set[Tuple[int, int]] = set()
+        for i, j in self.potential_moves[0]:
+            if self.__is_legal_move((i, j)):
+                black_legal.add((i, j))
+        for i, j in self.potential_moves[1]:
+            if self.__is_legal_move((i, j)):
+                white_legal.add((i, j))
+        self.legal_moves = (black_legal, white_legal)
